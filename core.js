8fbeec133fadbbcf14794900905bef5302fb5373
excing <courage.exc@gmail.com>
2022-08-23 20:29:40 +0800

    Update: 添加初始化资源（空方法）

--- core.js
+++ core.js
@@ -1,4 +1,24 @@
+
+function initRootsAndAffixesResource(errorFn) {
+  fetch('/find-roots-of-word/roots-and-affixes.csv', {
+    method: 'GET',
+  })
+    .then(response => {})
+    .catch(errorFn)
+}
+
+function initWordExchangesResource(errorFn) {
+  fetch('/find-roots-of-word/word-exchanges.csv', {
+    method: 'GET',
+  })
+    .then(response => {})
+    .catch(errorFn)
+}
+
+var rootsAndAffixesMap
+var wordExchangeMap
+
 // findRootAffixes is find root-affixes of word
-function findRootAffixes(word) {
-  return []  
+function findRootAffixes(word, errorFn) {
+  return []
 }
